3459f92937b32c4cb5789835cd317d71633669fb
lejam01 <ljardila650@misena.edu.co>
2022-09-01 17:00:58 -0500

    update intro-git/README.md and refactor intro-git/HolaMundo.java

--- intro-git/HolaMundo.java
+++ intro-git/HolaMundo.java
@@ -1,6 +1,10 @@
 class HolaMundo{
     public static void main (String[] args){
-        
+        saludar ("Leonardo");
+    }
+
+    public static void saludar(String nombre) {
+        System.out.println("Hola " +nombre  + "!");
     }
 
 }
